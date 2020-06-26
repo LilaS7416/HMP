869344476ec60738c54da9c96d49bcbc0913c394
linckandrea <linckandrea01@gmail.com>
2020-06-26 20:30:40 +0200

    pop-kernel setup

---
+++ compiler-script.sh
@@ -0,0 +1,9 @@
+make O=out ARCH=arm64 pop-kernel-poplar_defconfig
+
+PATH="/home/andrea/linux-x86/clang-r383902c/bin:/home/andrea/aarch64-linux-android-4.9/bin:/home/andrea/arm-linux-androideabi-4.9/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
