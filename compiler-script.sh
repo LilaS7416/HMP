2c0138f3841c4a9c8f73a66ab07a5e5028649a63
Andrea <linckandrea01@gmail.com>
2020-07-12 13:37:12 +0200

    compiler-script: update toolchain paths

--- compiler-script.sh
+++ compiler-script.sh
@@ -15,8 +15,8 @@ echo
 export CLANG_PATH=/home/andrea/linux-x86/clang-r383902c/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=/home/andrea/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=/home/andrea/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/andrea/linux-x86/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/home/andrea/linux-x86/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 echo
 echo "Set DEFCONFIG"
