488fd955ad63d12abd9978b0697c0c63ed3fe390
Andrea <linckandrea01@gmail.com>
2020-08-16 17:52:23 +0200

    irq: fix derp

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -358,6 +358,7 @@ irq_set_affinity_notifier(unsigned int irq, struct irq_affinity_notify *notify)
 			/* Pending work had a ref, put that one too */
 			kref_put(&old_notify->kref, old_notify->release);
 		}
+        kref_put(&old_notify->kref, old_notify->release);
 	}
 
 	return 0;
