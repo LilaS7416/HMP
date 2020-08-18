12f027f3b34c86cb28561b383f97fb3fae1962ea
Andrea <linckandrea01@gmail.com>
2020-08-18 18:01:00 +0200

    anykernel: maple: add pop init as well

--- AnyKernel/maple/anykernel.sh
+++ AnyKernel/maple/anykernel.sh
@@ -41,6 +41,22 @@ dump_boot;
 
 # begin ramdisk changes
 
+# init.qcom.power.rc
+# remove mpdecision
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "start mpdecision";
+
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/boost_ms 20";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/sync_threshold 1728000";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/input_boost_freq 1497600";
+remove_line "/system/vendor/etc/init/hw/init.qcom.rc" "write /sys/module/cpu_boost/parameters/input_boost_ms 40";
+
+replace_string "/system/vendor/etc/init/hw/init.qcom.rc" "230000" "300000" "230000" "global"
+
+# add Pop kernel Script
+ui_print "- Applying Pop kernel init patch in /system/vendor"
+cp /tmp/anykernel/ramdisk/init.pop.rc /system/vendor/etc/init/hw/init.pop.rc;
+insert_line "/system/vendor/etc/init/hw/init.qcom.rc" "init.pop.rc" after "import /vendor/etc/init/hw/init.target.rc" "import /vendor/etc/init/hw/init.pop.rc";
+chmod 644 /system/vendor/etc/init/hw/init.pop.rc;
 # end ramdisk changes
 
 write_boot;
